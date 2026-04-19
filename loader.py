fe3fabf53f7ed3a7db3ff8379750576ae4901ad0
Nefos1516 <netelev.d.p@edu.mirea.ru>
2026-04-19 20:36:37 +0300

    Deleted redundant files and added exception handler to load_data in loader.py

--- loader.py
+++ loader.py
@@ -5,11 +5,15 @@ def load_data(files: list[str]) -> list[dict]:
     data = []
 
     for file in files:
-        with open(file, newline="", encoding="utf-8") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                row["ctr"] = float(row["ctr"])
-                row["retention_rate"] = float(row["retention_rate"])
-                data.append(row)
-
+        try:
+            with open(file, newline="", encoding="utf-8") as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    row["ctr"] = float(row["ctr"])
+                    row["retention_rate"] = float(row["retention_rate"])
+                    data.append(row)
+        except FileNotFoundError:
+            print("The selected file does not exist")
+        except KeyError:
+            print('The selected file does not have needed keys')
     return data
--- tests/test_empty_file.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from loader import load_data
-
-
-def test_file_not_found():
-    with pytest.raises(FileNotFoundError):
-        load_data(["nonexistent.csv"])
